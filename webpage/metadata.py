27bc6e7484ced2d1d30bd494551ff2a625fd8f74
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-10-17 10:09:31 +0200

    initial commit

---
+++ webpage/metadata.py
@@ -0,0 +1,13 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'Handke in Zungen',
+    'author': 'Peter Andorfer, Vanessa Hannesschläger',
+    'subtitle': 'Forschungsprojekt zu Fremdsprachen in den Bühnentexten von Peter Handke',
+    'description': """Seit den beginnenden 1980er Jahren haben Fremdsprachen in den Bühnentexten des österreichischen Schriftstellers Peter Handke (*1942) zunehmend an Bedeutung gewonnen. In den frühen, sprachkritischen Stücken der 1960er und 70er Jahre spielen die Sprache, ihre Gemachtheit und die Reflexion darüber die zentrale Rolle – mit einem Umschwung, der sich am “dramatischen Gedicht” Über die Dörfer (1981) festgemachen lässt, werden die Bühnenarbeiten Handkes zunehmend “erzählend”, gewinnen zunehmend an “Handlung”. Mit dieser “Wende”,[2] mit der auch der Beginn von Handkes Tätigkeit als Übersetzer einhergeht, halten auch die fremden Sprachen Einzug in die Stücke des Autors. In diesem Projekt werden sämtliche fremdsprachigen Wörter und Textteile in den über 20 Bühnentexten Handkes erhoben, ausgewertet und untersucht. Dabei sind die Leitfragen, ob und in welcher Weise bestimmte Sprachen für bestimmte semantische Felder und Themenbereiche eingesetzt werden, welche Sprachen vorherrschen, ob und wie sich die Wichtigkeit einzelner Sprachen im Lauf der Zeit verändert und wie die verschiedenen einfließenden Fremdsprachen miteinander in Beziehung stehen.""",
+    'github': 'https://github.com/acdh-oeaw/hiz',
+    'purpose_de': 'Ziel von "Handke in Zungen" ist die Publikation von Forschungsdaten.',
+    'purpose_en': 'The purpose of "Handke in Zungen" is the publication of research data.',
+    'version': '1.0.0'
+}
