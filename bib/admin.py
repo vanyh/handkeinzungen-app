ce29424672bd0587acef90d2eb49d3e47cb5a14a
Vanessa Hannesschläger <vanessa.hannesschlaeger@oeaw.ac.at>
2017-10-23 17:01:06 +0200

    working towards #2

--- bib/admin.py
+++ bib/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Book, TextChunk, Quote
+from .models import Book, PartOfQuote, Quote, Work, Speaker
 
 admin.site.register(Book)
-admin.site.register(TextChunk)
+admin.site.register(PartOfQuote)
 admin.site.register(Quote)
+admin.site.register(Work)
+admin.site.register(Speaker)
--- bib/models.py
+++ bib/models.py
@@ -5,19 +5,29 @@ from vocabs.models import SkosConcept
 from places.models import Place
 
 
+class Person(IdProvider):
+    """Eine Person, zum Beispiel die Verfasserin eines Werks."""
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    person_gnd = models.CharField(max_length=100, blank=True, null=True)
+
+    def __init__(self):
+        return "{}, {}".format(self.last_name, self.first_name)
+
+
 class Book(IdProvider):
     zoterokey = models.CharField(max_length=100, blank=True, null=True)
     item_type = models.CharField(max_length=100, blank=True, null=True)
-    author = models.CharField(max_length=100, blank=True, null=True)
+    book_author = models.ManyToManyField(Person, blank=True)
     title = models.CharField(max_length=500, blank=True, null=True)
     publication_title = models.CharField(max_length=100, blank=True, null=True)
     short_title = models.CharField(max_length=500, blank=True, null=True)
     publication_year = models.IntegerField(blank=True, null=True)
     pub_place = models.ManyToManyField(Place, blank=True)
-    isbn = models.CharField(max_length=100, blank=True, null=True)
-    issn = models.CharField(max_length=100, blank=True, null=True)
-    doi = models.CharField(max_length=100, blank=True, null=True)
-    url = models.CharField(max_length=100, blank=True, null=True)
+    isbn = models.CharField(max_length=500, blank=True, null=True)
+    issn = models.CharField(max_length=500, blank=True, null=True)
+    doi = models.CharField(max_length=500, blank=True, null=True)
+    book_gnd = models.CharField(max_length=500, blank=True, null=True)
     book_type = models.ManyToManyField(SkosConcept, blank=True)
 
     def get_zotero_url(self):
@@ -31,27 +41,56 @@ class Book(IdProvider):
         return "{}, {}".format(self.author, self.title)
 
 
-class TextChunk(IdProvider):
+class Work(IdProvider):
+    work_author = models.ManyToManyField(Person, blank=True)
+    title = models.CharField(
+        verbose_name="Titel", max_length=500, blank=True, null=True,
+        help_text="Geben Sie hier den Titel des publizierten Werks ein")
+    alt_title = models.ManyToManyField(SkosConcept, blank=True)
+    creation_start_date = models.DateField(blank=True, null=True)
+    creation_end_date = models.DateField(blank=True, null=True)
+    start_date_sure = models.BooleanField(default=True)
+    end_date_sure = models.BooleanField(default=True)
+    creation_place = models.ManyToManyField(Place, blank=True)
+    published_in = models.ManyToManyField(Book, blank=True)
+    work_type = models.ManyToManyField(SkosConcept, blank=True)
+
+    def __str__(self):
+        return "Work: {}".format(self.title)
+
+
+class Speaker(IdProvider):
+    name = models.CharField(max_length=500, blank=True, null=True)
+    definition = models.CharField(max_length=500, blank=True, null=True)
+    alt_name = models.ManyToManyField(SkosConcept, blank=True)
+
+
+class Quote(IdProvider):
     """Provides the context of quotes"""
     book_source = models.ForeignKey(Book, blank=True, null=True)
     startpage = models.IntegerField(blank=True)
     endpage = models.IntegerField(blank=True)
     text = models.TextField(blank=True)
-    chunk_type = models.ManyToManyField(SkosConcept, blank=True)
+    quote_type = models.ManyToManyField(SkosConcept, blank=True)
+    part_of = models.ManyToManyField('self', blank=True)
+    auto_trans = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
         return "{}".format(self.text)
 
 
-class Quote(IdProvider):
+class PartOfQuote(IdProvider):
     """A class modeling quotes"""
     text = models.CharField(blank=True, max_length=500)
-    part_of = models.ManyToManyField(TextChunk, blank=True)
-    source = models.ForeignKey(Book, blank=True, null=True)
+    part_of = models.ManyToManyField(Quote, blank=True)
+    source = models.ForeignKey(Work, blank=True, null=True)
     follows = models.ManyToManyField('self', blank=True, related_name='has_follower')
     translates = models.ManyToManyField('self', blank=True, related_name='has_translation')
     language = models.ForeignKey(SkosConcept, blank=True, null=True, related_name='quote_language')
-    quote_type = models.ManyToManyField(SkosConcept, blank=True, related_name='quote_type')
+    partofquote_type = models.ManyToManyField(SkosConcept, blank=True, related_name='quote_type')
+    speaker = models.ManyToManyField(Speaker, blank=True)
+
+
 
     def __str__(self):
         return "{}".format(self.text)
