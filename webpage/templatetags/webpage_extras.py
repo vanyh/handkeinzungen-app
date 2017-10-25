5e168fba3d8e74136f0b0c422b7d94d6d0e9ad64
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-10-25 17:24:24 +0200

    added (reusable) count objects template tag (and implemented it in a probably very annoying way)

--- webpage/templatetags/webpage_extras.py
+++ webpage/templatetags/webpage_extras.py
@@ -1,7 +1,35 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 from webpage.metadata import PROJECT_METADATA as PM
 
 register = template.Library()
+
+
+@register.simple_tag
+def create_object_count(app='bib'):
+
+    """fetches all models of the passed in app and returns a
+    dict containg the name of each class and the number of instances"""
+
+    models = ContentType.objects.filter(app_label=app)
+    result = []
+    for x in models:
+        modelname = x.name
+        modelname = modelname.replace(" ", "").lower()
+        try:
+            fetched_model = ContentType.objects.get(app_label=app, model=modelname).model_class()
+            item = {
+                'name': modelname.title(),
+                'count': fetched_model.objects.count()
+            }
+            result.append(item)
+        except:
+            item = {
+                'name': x,
+                'count': "Some error occured"
+            }
+            result.append(item)
+    return result
 
 
 @register.simple_tag
