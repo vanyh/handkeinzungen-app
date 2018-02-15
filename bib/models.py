1633476356f7f429213c04b6c3db41a5c3b7013a
aureon249 <Martin.Kirnbauer@oeaw.ac.at>
2018-02-15 14:21:35 +0100

    added translations

--- bib/models.py
+++ bib/models.py
@@ -12,13 +12,13 @@ class Person(IdProvider):
     """Eine Person, zum Beispiel die Verfasserin eines Werks."""
     first_name = models.CharField(
         max_length=100, blank=True, null=True,
-        verbose_name="Vorname der Person")
+        verbose_name=ugettext_lazy("Vorname der Person"))
     last_name = models.CharField(
         max_length=100, blank=True, null=True,
-        verbose_name="Nachname der Person")
+        verbose_name=ugettext_lazy("Nachname der Person"))
     person_gnd = models.CharField(
         max_length=100, blank=True, null=True,
-        verbose_name="GND-ID der Person")
+        verbose_name=ugettext_lazy("GND-ID der Person"))
 
     @classmethod
     def get_createview_url(self):
@@ -56,33 +56,33 @@ class Person(IdProvider):
 class Book(models.Model):
     """Bibliographische Informationen zu einem publizierten Buch."""
     zoterokey = models.CharField(
-        verbose_name="Zoterokey", primary_key=True, max_length=100, blank=True,
-        help_text="Zoterokey des bibliographischen Eintrags aus der Zoterolibrary 'Peter Handke stage texts'")
+        verbose_name=ugettext_lazy("Zoterokey"), primary_key=True, max_length=100, blank=True,
+        help_text=ugettext_lazy("Zoterokey des bibliographischen Eintrags aus der Zoterolibrary 'Peter Handke stage texts'"))
     item_type = models.CharField(
         max_length=100, blank=True, null=True,
-        verbose_name="Item type")
+        verbose_name=ugettext_lazy("Objekttyp"))
     author = models.CharField(
         max_length=250, blank=True, null=True,
-        verbose_name="Autor*innenname laut Zotero-Eintrag"
+        verbose_name=ugettext_lazy("Autor*innenname laut Zotero-Eintrag")
     )
     book_author = models.ManyToManyField(
-        Person, blank=True, verbose_name="Autor*innenname")
+        Person, blank=True, verbose_name=ugettext_lazy("Autor*innenname"))
     title = models.CharField(
-        max_length=500, blank=True, null=True, verbose_name="Titel des Werks")
+        max_length=500, blank=True, null=True, verbose_name=ugettext_lazy("Titel des Werks"))
     siglum = models.CharField(
-        verbose_name="Sigle", max_length=500, blank=True, null=True,
-        help_text="Sigle des publizierten Werks (laut Handkeonline)")
+        verbose_name=ugettext_lazy("Sigle"), max_length=500, blank=True, null=True,
+        help_text=ugettext_lazy("Sigle des publizierten Werks (laut Handkeonline)"))
     publication_title = models.CharField(max_length=100, blank=True, null=True)
     short_title = models.CharField(max_length=500, blank=True, null=True)
     publication_year = models.IntegerField(
-        blank=True, null=True, verbose_name="Jahr der Veröffentlichung")
+        blank=True, null=True, verbose_name=ugettext_lazy("Jahr der Veröffentlichung"))
     pub_place = models.ManyToManyField(
-        Place, blank=True, verbose_name="Ort der Veröffentlichung")
+        Place, blank=True, verbose_name=ugettext_lazy("Ort der Veröffentlichung"))
     book_gnd = models.CharField(
-        max_length=500, blank=True, null=True, verbose_name="GND-ID des Buchs")
+        max_length=500, blank=True, null=True, verbose_name=ugettext_lazy("GND-ID des Buchs"))
     item_type = models.CharField(
         max_length=500, blank=True, null=True,
-        verbose_name="Item type laut Zotero-Eintrag"
+        verbose_name=ugettext_lazy("Item type laut Zotero-Eintrag")
     )
     book_type = models.ManyToManyField(SkosConcept, blank=True)
 
@@ -103,7 +103,7 @@ class Book(models.Model):
             )
             return url
         except AttributeError:
-            return "please provide Zotero settings"
+            return ugettext_lazy("Bitte Zotero-Einstellungen bereitstellen")
 
     def __str__(self):
         return "{}, {}".format(self.author, self.title)
@@ -117,36 +117,36 @@ class Book(models.Model):
     def get_alternative_classname(self):
         """Returns the alternative name of the class.
         Needed to present the human readable name of class"""
-        return 'Bücher'
+        return ugettext_lazy('Bücher')
 
 
 class Work(IdProvider):
     """Ein Werk an sich (unabhängig von seiner Nicht-/Publikation)"""
     work_author = models.ManyToManyField(
         Person, blank=True, related_name="has_work_created",
-        verbose_name= ugettext_lazy("Work author"))
+        verbose_name=ugettext_lazy("Autor*in des Werks"))
     work_translator = models.ManyToManyField(
         Person, blank=True, related_name="has_work_translated",
-        verbose_name="Übersetzer*in des werks"
+        verbose_name=ugettext_lazy("Übersetzer*in des werks")
     )
     title = models.CharField(
-        verbose_name="Titel des Werks", max_length=500, blank=True, null=True)
+        verbose_name=ugettext_lazy("Titel des Werks"), max_length=500, blank=True, null=True)
     alt_title = models.ManyToManyField(
-        SkosConcept, blank=True, related_name="is_work_alt_title", verbose_name="Titelvarianten")
-    creation_start_date = models.DateField(blank=True, null=True, verbose_name="entstanden von")
-    creation_end_date = models.DateField(blank=True, null=True, verbose_name="entstanden bis")
-    start_date_sure = models.BooleanField(default=True, verbose_name="Anfangsdatum gesichert")
-    end_date_sure = models.BooleanField(default=True, verbose_name="Enddatum gesichert")
-    creation_place = models.ManyToManyField(Place, blank=True, verbose_name="Entstehungsort")
-    creation_place_sure = models.BooleanField(default=True, verbose_name="Entstehungsort gesichert")
+        SkosConcept, blank=True, related_name="is_work_alt_title", verbose_name=ugettext_lazy("Titelvarianten"))
+    creation_start_date = models.DateField(blank=True, null=True, verbose_name=ugettext_lazy("entstanden von"))
+    creation_end_date = models.DateField(blank=True, null=True, verbose_name=ugettext_lazy("entstanden bis"))
+    start_date_sure = models.BooleanField(default=True, verbose_name=ugettext_lazy("Anfangsdatum gesichert"))
+    end_date_sure = models.BooleanField(default=True, verbose_name=ugettext_lazy("Enddatum gesichert"))
+    creation_place = models.ManyToManyField(Place, blank=True, verbose_name=ugettext_lazy("Entstehungsort"))
+    creation_place_sure = models.BooleanField(default=True, verbose_name=ugettext_lazy("Entstehungsort gesichert"))
     published_in = models.ManyToManyField(
-        Book, blank=True, related_name="publication_of_work", verbose_name="veröffentlicht in")
-    work_type = models.ManyToManyField(SkosConcept, blank=True, verbose_name="Werktyp")
+        Book, blank=True, related_name="publication_of_work", verbose_name=ugettext_lazy("veröffentlicht in"))
+    work_type = models.ManyToManyField(SkosConcept, blank=True, verbose_name=ugettext_lazy("Werktyp"))
     main_language = models.ForeignKey(
         SkosConcept, null=True, blank=True, related_name="language_of_work",
-        verbose_name="dominante Schreibsprache")
+        verbose_name=ugettext_lazy("dominante Schreibsprache"))
     has_translation = models.ForeignKey(
-        'self', blank=True, null=True, related_name="is_translation_of", verbose_name="Übersetzung"
+        'self', blank=True, null=True, related_name="is_translation_of", verbose_name=ugettext_lazy("Übersetzung")
     )
 
     @classmethod
@@ -175,7 +175,7 @@ class Work(IdProvider):
     def get_alternative_classname(self):
         """Returns the alternative name of the class.
         Needed to present the human readable name of class"""
-        return 'Werke'
+        return ugettext_lazy('Werke')
 
     @classmethod
     def get_createview_url(self):
@@ -184,12 +184,12 @@ class Work(IdProvider):
 
 class Speaker(IdProvider):
     """Figur, die spricht"""
-    name = models.CharField(max_length=500, blank=True, null=True, verbose_name="Figurenname")
+    name = models.CharField(max_length=500, blank=True, null=True, verbose_name=ugettext_lazy("Figurenname"))
     definition = models.CharField(
-        max_length=500, blank=True, null=True, verbose_name="weitere Informationen zur Figur"
+        max_length=500, blank=True, null=True, verbose_name=ugettext_lazy("weitere Informationen zur Figur")
     )
     alt_name = models.ManyToManyField(
-        SkosConcept, blank=True, verbose_name="alternative Figurenbezeichnungen"
+        SkosConcept, blank=True, verbose_name=ugettext_lazy("alternative Figurenbezeichnungen")
     )
 
     def distinct_rel_works(self):
@@ -219,7 +219,7 @@ class Speaker(IdProvider):
     def get_alternative_classname(self):
         """Returns the alternative name of the class.
         Needed to present the human readable name of class"""
-        return 'Sprecher'
+        return ugettext_lazy('Sprecher')
 
     @classmethod
     def get_createview_url(self):
@@ -248,16 +248,16 @@ class Speaker(IdProvider):
 class Quote(IdProvider):
     """ein Zitat, das auch Kontext zu fremdsprachigen Textstellen beinhaltet"""
     book_source = models.ForeignKey(
-        Book, blank=True, null=True, related_name="has_quotes", verbose_name="Quelle"
+        Book, blank=True, null=True, related_name="has_quotes", verbose_name=ugettext_lazy("Quelle")
     )
-    startpage = models.IntegerField(blank=True, null=True, verbose_name="Seite (von)")
-    endpage = models.IntegerField(blank=True, null=True, verbose_name="Seite (bis)")
-    text = models.TextField(blank=True, verbose_name="Text")
-    quote_type = models.ManyToManyField(SkosConcept, blank=True, verbose_name="Zitattyp")
+    startpage = models.IntegerField(blank=True, null=True, verbose_name=ugettext_lazy("Seite (von)"))
+    endpage = models.IntegerField(blank=True, null=True, verbose_name=ugettext_lazy("Seite (bis)"))
+    text = models.TextField(blank=True, verbose_name=ugettext_lazy("Text"))
+    quote_type = models.ManyToManyField(SkosConcept, blank=True, verbose_name=ugettext_lazy("Zitattyp"))
     part_of = models.ManyToManyField(
-        'self', blank=True, verbose_name="Teil von Zitat",
-        help_text="Dieses Zitat ist Teil eines umfangreicheren Zitats")
-    auto_trans = models.ManyToManyField('self', blank=True, verbose_name="Selbstübersetzung")
+        'self', blank=True, verbose_name=ugettext_lazy("Teil von Zitat"),
+        help_text=ugettext_lazy("Dieses Zitat ist Teil eines umfangreicheren Zitats"))
+    auto_trans = models.ManyToManyField('self', blank=True, verbose_name=ugettext_lazy("Selbstübersetzung"))
 
     def distinct_rel_languages(self):
         part_of_quote = self.has_chunks.all().distinct()
@@ -298,7 +298,7 @@ class Quote(IdProvider):
     def get_alternative_classname(self):
         """Returns the alternative name of the class.
         Needed to present the human readable name of class"""
-        return 'Zitate'
+        return ugettext_lazy('Zitate')
 
     @classmethod
     def get_createview_url(self):
@@ -307,27 +307,27 @@ class Quote(IdProvider):
 
 class PartOfQuote(IdProvider):
     """fremdsprachiger Zitatteil"""
-    text = models.CharField(blank=True, max_length=500, verbose_name="Text")
+    text = models.CharField(blank=True, max_length=500, verbose_name=ugettext_lazy("Text"))
     part_of = models.ForeignKey(
         Quote, blank=True, null=True, related_name="has_chunks",
-        verbose_name="Teil von Zitat",
-        help_text="Dieses Teilzitat ist Teil eines umfangreicheren Zitats")
-    source = models.ForeignKey(Work, blank=True, null=True, verbose_name="Quelle")
+        verbose_name=ugettext_lazy("Teil von Zitat"),
+        help_text=ugettext_lazy("Dieses Teilzitat ist Teil eines umfangreicheren Zitats"))
+    source = models.ForeignKey(Work, blank=True, null=True, verbose_name=ugettext_lazy("Quelle"))
     follows = models.ForeignKey(
         'self', blank=True, null=True, related_name='has_follower',
-        verbose_name="folgt auf", help_text="Dieses Teilzitat folgt einem anderen")
+        verbose_name=ugettext_lazy("folgt auf"), help_text=ugettext_lazy("Dieses Teilzitat folgt einem anderen"))
     translates = models.ManyToManyField(
-        'self', blank=True, related_name='has_translation', verbose_name="Übersetzung",
-        help_text="Dieses Teilzitat hat eine Übersetzung in unmittelbarer Umgebung innerhalb des Texts"
+        'self', blank=True, related_name='has_translation', verbose_name=ugettext_lazy("Übersetzung"),
+        help_text=ugettext_lazy("Dieses Teilzitat hat eine Übersetzung in unmittelbarer Umgebung innerhalb des Texts")
     )
     correct_translation_sure = models.BooleanField(
-        default=True, verbose_name="Übersetzung korrekt")
+        default=True, verbose_name=ugettext_lazy("Übersetzung korrekt"))
     language = models.ForeignKey(
-        SkosConcept, blank=True, null=True, related_name='quote_language', verbose_name="Sprache")
+        SkosConcept, blank=True, null=True, related_name='quote_language', verbose_name=ugettext_lazy("Sprache"))
     partofquote_type = models.ManyToManyField(
-        SkosConcept, blank=True, related_name='quote_type', verbose_name="Teilzitattyp")
+        SkosConcept, blank=True, related_name='quote_type', verbose_name=ugettext_lazy("Teilzitattyp"))
     speaker = models.ManyToManyField(
-        Speaker, blank=True, verbose_name="Figur", related_name="speaks"
+        Speaker, blank=True, related_name="speaks", verbose_name=ugettext_lazy("Figur")
     )
 
     def get_absolute_url(self):
@@ -356,7 +356,7 @@ class PartOfQuote(IdProvider):
     def get_alternative_classname(self):
         """Returns the alternative name of the class.
         Needed to present the human readable name of class"""
-        return 'Zitat-Teile'
+        return ugettext_lazy('Zitat-Teile')
 
     @classmethod
     def get_createview_url(self):
